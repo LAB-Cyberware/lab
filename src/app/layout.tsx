7bb1ebdddd4d25ce669317641419811ea39f39f5
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2025-08-04 13:41:45 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,10 @@ import FooterClient from "@/components/FooterClient";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <head><meta name="google-site-verification" content="3ywNHIOQHjrxUF43eHALVp3v9m2k5xnYBnkUXyYmUiI" /></head>
+      <head>
+        <meta name="google-site-verification" content="3ywNHIOQHjrxUF43eHALVp3v9m2k5xnYBnkUXyYmUiI" />
+        <meta name="facebook-domain-verification" content="wnnrhtsydmmog5wl223j29osk9u98u" />
+      </head>
       <body>
         <Providers>
           <Navbar />
