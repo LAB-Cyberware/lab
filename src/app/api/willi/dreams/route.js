2b52404c0ccbc2014ca7591fea2a18f869ec9c8a
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2025-09-02 13:15:32 -0400

    dreams

---
+++ src/app/api/willi/dreams/route.js
@@ -0,0 +1,126 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextResponse } from "next/server";
+import getPrompt from "@/ia-utils/templates-Prompts";
+import jsonPure from "@/utils/jsonPure";
+import jsonToPrompt from "@/utils/JsonToPrompt";
+
+//gemini-pro-vision
+      //gemini-2.0-flash-preview-image-generation
+      import {
+        GoogleGenAI,
+      } from '@google/genai';    
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY || "");
+const model = genAI.getGenerativeModel({
+  model: process.env.GOOGLE_GEMINI_API_MODET_TEXT,
+  system_instruction: {
+    parts: [
+      {
+        text: `Eres una IA experta en Marketing, Neuroventas, Psicología, Sociología, Optimización de Recursos y Administración de Empresas`
+      }
+    ]
+  },
+  tools: [
+    {
+      codeExecution: {},
+    },
+    
+  ]
+});
+
+
+export async function POST(req) {
+  const dataR = await req.json();
+  //console.log("@@@@@@@ POST POST ")
+  //console.log(dataR)
+  try {
+    
+    const finalPrompt = getPrompt(dataR.item,jsonToPrompt(dataR.maker),jsonToPrompt(dataR.estudio),jsonToPrompt(dataR.estrategia),jsonToPrompt(dataR.post));
+    if(dataR.item=="post-final-img"){
+        async function main() {
+        let info;
+        const ai = new GoogleGenAI({
+          apiKey: process.env.GOOGLE_GEMINI_API_KEY,
+        });
+        const config = {
+          responseModalities: [
+              'IMAGE',
+              'TEXT',
+          ],
+          responseMimeType: 'text/plain',
+          aspectRatio: "1:1",
+        };
+
+        
+        const model = process.env.GOOGLE_GEMINI_API_MODET_IMAGE;
+        const contents = [
+          {
+            role: 'user',
+            parts: [
+              {
+                text: `${finalPrompt}`,
+              },
+            ],
+          },
+        ];
+      
+        const response = await ai.models.generateContentStream({
+          model,
+          config,
+          contents,
+        });
+        for await (const chunk of response) {
+          if (!chunk.candidates || !chunk.candidates[0].content || !chunk.candidates[0].content.parts) {
+            continue;
+          }
+          if (chunk.candidates?.[0]?.content?.parts?.[0]?.inlineData) {
+          //  const fileName = `${process.env.POST_IMAGES_DIR}/${fileMetaName}`;
+            const inlineData = chunk.candidates[0].content.parts[0].inlineData;
+          //  const fileExtension = mime.getExtension(inlineData.mimeType || '');
+          //  const buffer = Buffer.from(inlineData.data || '', 'base64');
+          //  saveBinaryFile(`${fileName}.${fileExtension}`, buffer);
+            //savePostImg(`${fileName}.${fileExtension}`)
+            return inlineData
+          }
+          else {
+            info = chunk.text
+          //  console.log(chunk.text);
+          }
+        }
+      }
+      
+      const result_img = await main(); 
+      let williTxt = result_img;
+      console.log("WILLI : williTxtImg")
+      console.log(williTxt)
+      let willJSON = jsonPure(williTxt)
+      let williArray = new Array();
+      williArray.push(JSON.parse(willJSON))
+      let data = williArray;
+
+      return NextResponse.json(data);
+    }else{
+      const result = await model.generateContent(finalPrompt); 
+      let williTxt = result.response.text()
+      
+      console.log("WILLI : williTxt")
+      console.log(williTxt)
+
+      
+     // console.log("++++++++ WILLI Generate content say : williTxt +++++++++")
+     // console.log(williTxt)
+
+      let willJSON = jsonPure(williTxt)
+      let williArray = new Array();
+      williArray.push(JSON.parse(willJSON))
+      let data = williArray[0];
+      //console.log("++++++++ WILLI Generate content say : data +++++++++")
+      //console.log(data)
+      return NextResponse.json(data);
+    }
+
+  } catch (error) {
+   // console.error("Error generating content:", error);
+    return NextResponse.json({ error: `Failed to generate content ${error}` }, { status: 500 });
+  }
+}
